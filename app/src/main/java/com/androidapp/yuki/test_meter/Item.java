0dc6e1e1f39a962d94f9a5a77950f1d78c35e770
mikaduki <mikaduki.0510@gmail.com>
2014-12-07 01:42:53 +0900

    Initial commit

---
+++ app/src/main/java/com/androidapp/yuki/test_meter/Item.java
@@ -0,0 +1,10 @@
+package com.androidapp.yuki.test_meter;
+
+/**
+ * Created by yuki on 2014/12/06.
+ */
+public class Item {
+    public String Id;
+    public String Text;
+    public String Step;
+}
---
+++ app/src/main/java/com/androidapp/yuki/test_meter/MainActivity.java
@@ -0,0 +1,69 @@
+package com.androidapp.yuki.test_meter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.TableOperationCallback;
+
+import java.net.MalformedURLException;
+
+
+public class MainActivity extends Activity {
+    private MobileServiceClient mClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            mClient = new MobileServiceClient(
+                    "https://loginapp.azure-mobile.net/",
+                    "doKbFBDFJVdevjXxxtlEVrGCLtuTXe22",
+                    this
+            );
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Item item = new Item();
+        item.Text = "すばらしいアイテム";
+        mClient.getTable(Item.class).insert(item, new TableOperationCallback<Item>() {
+            public void onCompleted(Item entity, Exception exception, ServiceFilterResponse response) {
+                if (exception == null) {
+                    // Insert succeeded
+                } else {
+                    // Insert failed
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
